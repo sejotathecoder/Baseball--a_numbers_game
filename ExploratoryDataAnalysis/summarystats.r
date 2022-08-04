ac426dfeea145c177fd391b46ff7c6278e1c8662
collinpucher <pucher@uw.edu>
2022-08-04 12:19:05 -0700

    summary stats

--- ExploratoryDataAnalysis/summarystats.r
+++ ExploratoryDataAnalysis/summarystats.r
@@ -3,4 +3,5 @@ library(tidyverse)
 all_1920_48_qb <- read.csv("data/NegroLeagueBatters1920to1948.csv")
 all_1920_48_qp <- read.csv("data/NegroLeaguePitchers1920to1948.csv")
 
-all
+bat_summary <- summary(all_1920_48_qb)
+pitcher_summary <- summary(all_1920_48_qp)
