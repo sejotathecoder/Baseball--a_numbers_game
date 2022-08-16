8b605d1fc0258623954349d6945a5342b04a6425
collinpucher <pucher@uw.edu>
2022-08-16 12:33:32 -0700

    added batter dataset

--- app.r
+++ app.r
@@ -3,12 +3,23 @@ library(plotly)
 library(fmsb)
 library(shiny)
 
+# Bringing hitting statistics in
+processed_bats <- read.csv("data/processed_bats.csv")
+just_batters <- processed_bats[4:867,]
+bat_list <- setNames(as.numeric(just_batters$X), just_batters$Player)
+
+
 home_page <- tabPanel("Home",
   
 )
 
 hitting_radar_page <- tabPanel("Hitting Radar Charts",
-
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("hitter_select", label = h3("Select a hitter"),
+                  choices = bat_list)
+    )
+  )
 )
 
 pitching_radar_page <- tabPanel("Pitching Radar Charts",
